488ee3564a17290c5ce208db0168dce88d717744
LeandroDolensi <leandro.dolensi@aluno.ifsp.edu.br>
2025-10-29 08:54:47 -0300

    feat: add routers for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Create from "./components/Create";
+import UpdatePage from "./components/UpdatePage";
+import Read from "./components/Read";
+import DeletePage from "./components/DeletePage";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export const API_URL = "https://6901eb2bb208b24affe43fdc.mockapi.io/movies";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <BrowserRouter>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Catálogo de Filmes
+          </Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Início
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/create">
+                  Criar
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/update">
+                  Alterar
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/delete">
+                  Apagar
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
-export default App
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/update" element={<UpdatePage />} />
+          <Route path="/delete" element={<DeletePage />} />
+          <Route path="/read/:id" element={<Read />} />
+        </Routes>
+      </div>
+      <footer>PW2 - Avaliação II - Leandro - SC301455X</footer>
+    </BrowserRouter>
+  );
+}
+export default App;
